dc019bf88b51737a1a41ab4c118bc0dc630ad9e8
calvertzhu <zhucalvert@gmail.com>
2025-02-26 17:31:41 -0500

    Update Car.hpp

--- include/Car.hpp
+++ include/Car.hpp
@@ -2,13 +2,24 @@
 
 #include <memory>
 
+#ifndef CAR_HPP
 class Car {
  private:
   /* Define car parameters here according to the doc */
+  // Private methods for calculating each loss/gain component
+  
+    double calc_aero_loss(double velocity);
+    double calc_rolling_loss(double velocity);
+    double calc_gravity_loss(double velocity, double angle);
+    double calc_solar_gain(double irradiance);
 
  public:
   Car();
-
+  // Energy consumption calculation (W)
+  double energy_consumption(double velocity, double angle, double irradiance);
+  
   // Define energy loss functions
 
 };
+
+#endif // CAR_HPP
